2a08067da0957670c58d39d4276102068c828242
Eddnie Alcaraz <edd.alcaraz@gmail.com>
2026-08-01 21:24:50 +0800

    update env variables

--- lk_website_ng/src/environments/environment.ts
+++ lk_website_ng/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  siteUrl: 'https://lkstudio.ph',
+  siteUrl: 'https://engravedbylk.com',
   siteName: 'LK Engraving Studio',
   contactEmail: 'lkstudioph@gmail.com',
   shopUrl: 'https://shopee.ph/shop/1152312396',
